6ad4d764508809d8ecb8c88ec4ad68bed1ab9226
guilhermemap <86965518+guilhermemap@users.noreply.github.com>
2022-11-14 23:11:08 -0300

    início

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+//senha vazia
+const sequelize = new Sequelize("nodemvc2", 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+try {
+    sequelize.authenticate();
+    console.log('conectado ao BD');
+} catch (error) {
+    console.log(`não foi possível conectar: ${error}`);
+}
+
+exports.default = sequelize;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const port = 3000;
+const app = express();
+const conn = require('./db/conn');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`ouvindo porta ${port}`);
+});
